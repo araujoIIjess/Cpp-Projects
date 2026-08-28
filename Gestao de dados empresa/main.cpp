b04f32afcf9b76b73f39d03d02ea3bc68bbb521e
Jess <jessemanuelnetoaraujo2@gmail.com>
2026-08-28 11:54:42 +0100

    Ínicio do sistema de inserir e validar nif

--- Gestao de dados empresa/main.cpp
+++ Gestao de dados empresa/main.cpp
@@ -1,39 +1,79 @@
 #include<iostream>
 #include<array>
 #include<string>
-
-int main(){
-    //Variavel para gurardar o nome do cliente
+#include<cstdint>
+//unção para obter o nome do cliente
+std::string getClientName(){
+    //nome do cliente
     std::string client_name {};
-    
-    //Variavel para guardar o email do cliente
+
+    //Pedindo os dados do cliente e validando input
+    do{
+        std::cout << "Insira o nome: " << '\n';
+        std::getline(std::cin, client_name);
+    //Se a string estiver vazia repete o loop
+    }while(client_name == "");
+
+    return client_name;
+}
+
+std::string getClientmail(){
+    //email do cliente
     std::string client_email {};
+
+    //validação do email
     bool found {};
 
-    //Loop pedindo o nome do cliente
+    //Pedindo o email e validando
     do{
+        std::cout << "Insira o email: " << '\n';
+        std::getline(std::cin, client_email);
+        //Verificando o dominio do email
+        /*
+            Se não tiver "@gmail" no email inserido, found retorna true. Se tiver "@gmail" retorna false
+            Se found for false o loop termina. Mas se for true o loop recomeça
+        */
+        found = client_email.find("@gmail") == std::string::npos;
+
+    }while(client_email == "" || found);
+
+    return client_email;
+
+}
+
+int main(){
+    //Variavel que pede e guarda o nome do cliente
+    std::string client_name {getClientName()};
+    //Variavel que pede e guarda o email do cliente
+    std::string client_email {getClientmail()};
+    //Variável que guarda o NIF do cliente
+    uint64_t client_nif {00000000000000};
+
+
+
+    //Loop pedindo o nome do cliente
+   /*do{
         //O loop se repete enquanto client_name for uma string vazia
         std::cout << "Insira o nome: ";
         //Pegando o nome e pondo na variavel
         std::getline(std::cin , client_name);//O std::ws serve para o getline ignorar os espaços em branco
-    }while(client_name == "");
+    }while(client_name == "");*/
 
     //Pedindo o email do cliente
     
     //Loop pedindo o email do cliente
-    do{
+    /*do{
         //Loop se repete enquanto client_email for uma string vazia
         std::cout << "Insira o email do cliente: \n";
         //Adicionando o valor a variavel
         std::getline(std::cin, client_email);
 
-        //Verificando o '@' no email && verificando o "gmail" no email
-        //Retorna true se não encontrar o '@' e o "gmail" e false se não os encontrar
-        found = client_email.find("@gmail.com") == std::string::npos;//(client_email.find('@') == std::string::npos || client_email.find("gmail") == std::string::npos);
+        //Verificando o "@gmail.com" no email
+        found = client_email.find("@gmail.com") == std::string::npos;
 
     //Condição para repetir o loop: não ter '@' no email ou o espaço estar vazio
     }while(client_email == "" || found);
 
-    std::cout << "email: " << client_email;
+    std::cout << "email: " << client_email;*/
     return 0;
 }
